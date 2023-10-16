51efaaac39d10cfb98db8849c23ef82ece8aebc6
Cheryl Chang <68939820+chihyuchang21@users.noreply.github.com>
2023-10-16 21:27:03 +0800

    Create 35.Search Insert Position.py

---
+++ 35.Search Insert Position.py
@@ -0,0 +1,15 @@
+# an algorithm with O(log n) runtime complexity --> using binary method
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2 
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left 
