ee340d3340135150a77da64e61e2ee3ba998a480
Chih-Yu Chang <chihyuchang21@gmail.com>
2023-12-19 12:04:25 +0800

    TBC: method(2)-dictionary

---
+++ 020. Valid Parentheses.py
@@ -0,0 +1,24 @@
+#使用stack比dictionary省空間
+#append它的另一半進去，酷
+
+class Solution:
+    def isValid(self,s):
+        stack = []
+
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item == '{':
+                stack.append('}')
+            elif item == '[':
+                stack.append(']')
+            elif not stack or stack[-1] != item:
+                return False
+            else:
+                stack.pop()
+
+            return True if not stack else False
+
+
+
+
