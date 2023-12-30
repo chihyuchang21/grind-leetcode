6546138ae93bcb98d79c3873c8664a06b2fcd0ec
Chih-Yu Chang <chihyuchang21@gmail.com>
2023-12-30 21:26:52 +0800

    Today's Progress(2/2): 654 using slice

--- 0106. Construct Binary Tree from Inorder and Postorder Traversal.py
+++ 0106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -1,10 +1,39 @@
 # Definition for a binary tree node.
 class TreeNode(object):
-def __init__(self, val=0, left=None, right=None):
-    self.val = val
-    self.left = left
-    self.right = right
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution(object):
     def buildTree(self, inorder, postorder):
+
+        #Step1: 特殊情形討論 - 樹為空、recursion終止條件
+        if not postorder:
+            return None
+
+        #Step2: postorder的最後一個就是當前的中間節點
+        root_val = postorder[-1]
+        root = TreeNode(root_val)
+
+        #Step3: 找切割點
+        separator_idx = inorder.index(root_val)
+
+        #Step4: 切割inorder數組，得到inorder數組的左,右半邊
+        inorder_left = inorder[:separator_idx]
+        inorder_right = inorder[separator_idx + 1:]
+
+        #Step5: 切割postorder數組，得到postorder數組的左,右半邊
+        #重點: 中序數組大小一定跟後序數組大小是相同的
+        postorder_left = postorder[:len(inorder_left)]
+        postorder_right = postorder[len(inorder_left): len(postorder) - 1]
+
+        #Step6: Recursion
+        root.left = self.buildTree(inorder_left, postorder_left)
+        root.right = self.buildTree(inorder_right, postorder_right)
+
+        #Step7: return answer
+        return root
+
+
 
---
+++ 0654. Maximum Binary Tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# 使用切片
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        if not nums:
+            return None
+
+        max_val = max(nums)
+        max_index = nums.index(max_val)
+        node = TreeNode(max_val)
+        node.left = self.constructMaximumBinaryTree(nums[:max_index])
+        node.right = self.constructMaximumBinaryTree(nums[max_index+1:])
+        return  node
