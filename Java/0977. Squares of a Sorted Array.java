44213be0033839c11023a4d14e41e7bb73a7d91e
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-02-08 15:21:47 +0800

    Today's Progress (209,977)

--- Java/0977. Squares of a Sorted Array.java
+++ Java/0977. Squares of a Sorted Array.java
@@ -1,6 +1,19 @@
+//Two-pointers
 class Solution{
-    public int[] sortedSqures(int[] nums){
+    public int[] sortedSquares(int[] nums){
 
+        int left = 0;
+        int right = nums.length - 1;
+        int[] result = new int[nums.length];
+        int index = nums.length - 1;
 
+        while(left <= right){
+            if(nums[left] * nums[left] > nums[right] * nums[right]){
+                result[index--] = nums[left] * nums[left++];
+            }else{
+                result[index--] = nums[right] * nums[right--];
+            }
+        }
+        return result;
     }
 }
