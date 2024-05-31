40e9746c630a7a75cda8ab867e002d9419044b07
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-05-31 23:22:54 +0800

    0001. Two Sum Sol2 (not yet finished)

--- Java/0001. Two Sum.java
+++ Java/0001. Two Sum.java
@@ -1,4 +1,63 @@
-//Brutal Force: Using 2 For Loop
+// Sol1: Brute Force
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null; // exactly one solution
+    }
+}
+
+// Sol2: HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+           Map<Integer, Integer> map = new HashMap<>();
+
+           for (int i = 0, i < nums.length, i++) {
+            deduction = target - nums[i];
+            if (map.containKey(deduction)) {
+                return
+            }
+
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Brute Force: Using 2 For Loop
 class Solution {
     public int[] twoSum(int[] nums, int target){
         for (int i = 0; i < nums.length; i++){
@@ -8,7 +67,20 @@ class Solution {
                 }
             }
         }
+        return new int[]{};
+    }
+}
 
-            return new int[]{};
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No Answer");
     }
 }
