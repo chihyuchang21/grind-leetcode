eca051161801cd5019cb7e104a7d13b2f0fb280d
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-06-23 16:09:26 +0800

    Today's Progress (203)

--- Java/0203. Remove Linked List Elements.java
+++ Java/0203. Remove Linked List Elements.java
@@ -9,23 +9,27 @@
  * }
  */
 
+
 // Practice Definition of Linked List
-public class ListNode {
-    int val;
-    ListNode next;
+//public class ListNode {
+//    int val;
+//    ListNode next;
+//
+//    public ListNode(){
+//    }
+//
+//    public ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    public ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
 
-    public ListNode(){
-    }
+//移除 head 跟移除 others 作法不同，要統一方法則設置 dummy head
 
-    public ListNode(int val) {
-        this.val = val;
-    }
-
-    public ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
@@ -37,6 +41,28 @@ class Solution {
         ListNode cur = head;
         while (cur != null){
             if (cur.val == val){
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+
+// 2nd 240623
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        // 設置虛擬指針放在最前面
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val == val) {   // head 所指的值 = val (target 的話)
                 pre.next = cur.next;
             } else {
                 pre = cur;
