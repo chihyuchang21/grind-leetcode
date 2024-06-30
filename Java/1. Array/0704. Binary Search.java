13dee6ad8a48d2a269b6b3571ad966c6bacc4d7b
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-06-30 19:37:00 +0800

    Today's Progress & Refactor

--- Java/1. Array/0704. Binary Search.java
+++ Java/1. Array/0704. Binary Search.java
@@ -21,13 +21,11 @@ class Solution {
 }
 
 
-
-
 // 240529 2nd: syntax and return still needed to be concerned
 class Solution {
     public int search (int[] nums, int target) {
         left = 0;
-        right = nums.length - 1
+        right = nums.length - 1;
         while (left < right) {
             mid = left + (right - left) / 2  // 防止 int overflow (if left = max - 2; right = max)
             if (nums[mid] < target) // if syntax
@@ -63,6 +61,27 @@ class Solution {
             }
         return - 1;
         }
+    }
+}
+
+//240630: 4th, <=, return position still (can't find)
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            }
+        }
+        return - 1;
     }
 }
 
