317b99fb9cded7b2c23ccc2fb80cac8f5b6899be
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-01 18:29:47 +0800

    Today's Progress (slow progress, way to go!)

---
+++ Java/1. Array/0056. Merge Intervals.java
@@ -0,0 +1,25 @@
+// https://www.youtube.com/watch?v=uiOq8TO5CbE
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return intervals;
+        List<int[]> res = new ArrayList<>();
+
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
+
+        int[] curr = intervals[0];
+
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > curr[1]) {
+                res.add(curr);
+                curr = intervals[i];
+            } else {
+                curr[1] = Math.max(curr[1], intervals[i][1]);
+            }
+        }
+
+        res.add(curr);
+
+        return res.toArray(new int[res.size()][2]);
+    }
+}
