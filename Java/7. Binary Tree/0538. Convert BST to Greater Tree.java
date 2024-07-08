0e9435bb12060c963423c1ac928f6e882cc0c5c7
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-08 17:03:29 +0800

    Today's Progress (1st Binary Tree Ended!)

---
+++ Java/7. Binary Tree/0538. Convert BST to Greater Tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        int sum = 0;
+        convertBST1(root);
+        return root;
+    }
+
+    // inorder traversal
+    public void convertBST1(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        convertBST1(root.right);
+        sum += root.val;
+        root.val = sum;
+        convertBST1(root.left);
+    }
+}
