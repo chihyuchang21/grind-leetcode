96d307884ff619ba23048c238381444e19227adc
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-16 15:24:58 +0800

    Today's Progress

--- Java/7. Binary Tree/Traversal (Tree Level Order)/0637. Average of Levels in Binary Tree.java
+++ Java/7. Binary Tree/Traversal (Tree Level Order)/0637. Average of Levels in Binary Tree.java
@@ -16,24 +16,25 @@
 
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> list = new ArrayList<>();
-        Deque<TreeNode> que = new LinkedList<>();
+        List<Double> resultList = new ArrayList<>(); // 用來裝答案
+        Deque<TreeNode> que = new LinkedList<>(); // 輔助用的 Deque
 
-        if (root == null) return list;
-        que.offerLast(root);
+        if (root == null) return resultList;
+        que.offerLast(root); // 在進入 while loop 前先把當前 root 家進 queue
 
         while (!que.isEmpty()) {
-            int levelSize = que.size();
-            double levelSum = 0.0;
+            int levelSize = que.size(); // 這層有幾個 elements
+            double levelSum = 0.0; // 加總先 initialized
+
             for (int i = 0; i < levelSize; i++) {
-                TreeNode poll = que.pollFirst();
-                levelSum += poll.val;
+                TreeNode poll = que.pollFirst(); // 先噴出第一個
+                levelSum += poll.val; // 加
 
                 if (poll.left != null) que.addLast(poll.left);
                 if (poll.right != null) que.addLast(poll.right);
             }
-            list.add(levelSum / levelSize);
+            resultList.add(levelSum / levelSize);
         }
-        return list;
+        return resultList;
     }
 }
