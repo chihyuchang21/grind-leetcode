f7ee8a4ed223517c8e3583c56487b63e973f857c
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-25 20:23:18 +0800

    Today's Progress (DP start)

---
+++ Java/10. Dynamic Programming/0070. Climbing Stairs.java
@@ -0,0 +1,34 @@
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n == 0 || n == 1)
+//             return 1;
+
+//         int[] dp = new int[n+1];
+//         dp [0] = dp[1] = 1;
+
+//         for (int i = 2; i <= n; i++) {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
+
+// 1. n = 1， result = 1
+// 2. n = 2, result = 1+1 (爬1階兩次 + 一次爬兩階)
+// 3. n = 3, result = 1+2 (前面兩個case相加)
+// 4. n = 4, result = 3+2 (前面兩個case相加)
+// 5. 發現其實這題就是算費氏數列
