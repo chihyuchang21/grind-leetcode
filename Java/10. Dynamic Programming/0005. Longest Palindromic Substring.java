f2b9df7d650e8b199450b5e8fee55ab7306d1cf1
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-07-26 13:06:46 +0800

    Today's Progress (DP)

---
+++ Java/10. Dynamic Programming/0005. Longest Palindromic Substring.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int strLength = s.length();
+
+        if (strLength < 2) {
+            return s;
+        }
+
+        int resultLength = 0;
+        String result = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd Length
+            int left = i, right = i;
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if ((right - left + 1) > resultLength) {
+                    result = s.subString(left, right + 1);
+                    resultLength = right - left + 1;
+                }
+                left -= 1;
+                right += 1;
+            }
+        }
+
+        // Even Length
+        left = i;
+        right = i + 1;
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            if ((right - left + 1) > resultLength) {
+                result = s.substring(left, right + 1);
+                resultLength = right - left + 1;
+            }
+            left -= 1;
+            right += 1;
+        }
+    }
+    return result;
+}
