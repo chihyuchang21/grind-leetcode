18e3328630ab142922f05662245d5db38c47726b
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-08-07 14:53:51 +0800

    Today's Progress

--- Java/1. Array/0001. Two Sum.java
+++ Java/1. Array/0001. Two Sum.java
@@ -30,7 +30,72 @@ class Solution {
     }
 }
 
-// recursive
+// 2nd August 24'
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int deduction = target - nums[i];
+            if (map.containsKey(deduction)) {
+             return new int[]{map.get(deduction),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
+
+
+/**
+ * 240807
+ *
+ * # Understand
+ * 1. Does only one answer exist?
+ * 2. Can the nums array be empty?
+ * 3. Is the array sorted
+ *
+ *
+ * # Match
+ * 1. Brute Force (Array)
+ *      Tc: O(N^2)
+ *      SC: O(1)
+ * 2. HashMap
+ *      TC: O(1)
+ *      SC: O(N)
+ *
+ * # Plan
+ * # Impliment
+ * # Review
+ * # Evaluate
+ *
+ * # Notes
+ * [Brute Force] - Great progress, keep it up!
+ * 0. Should use an online whiteboard to draw the graph first to ensure the basic settings for the for loop are correct.
+ * 1. The end condition of the for loop (i < nums.length)
+ * 2. The difference between int[] (array) and ArrayList
+ *      When using int[], we can initialize it as int[2] first, or we can use new int[]{i, j} directly (important)
+ * 3. Where to place return null result
+ * 4. How to create a new HashMap<Integer, Integer>
+ */
 
 
 
