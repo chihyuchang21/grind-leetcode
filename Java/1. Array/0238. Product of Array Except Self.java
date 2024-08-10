5df958217a8f2f2bded16a5a333922c58ca54a57
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-08-10 13:27:41 +0800

    Today's Progress

--- Java/1. Array/0238. Product of Array Except Self.java
+++ Java/1. Array/0238. Product of Array Except Self.java
@@ -17,23 +17,26 @@ class Solution {
     }
 }
 
-// 2. Product of all and then divide the product (smart!) -> O(N) -> 有被除數可能為 0 的可能
+// 2. Product of all and then divide the product (smart!) -> O(N) -> 有被除數可能為 0 的可能 (/ by zero)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int ans[] = new int[n];
-        int pro = 1;
+        int[] result = new int[nums.length];
 
-        for (int i : nums) {
-            pro *= i;
-        }
+        int product = 1;
 
         for (int i = 0; i < n; i++) {
-            ans[i] = pro / nums[i];
+            product *= nums[i];
         }
-        return ans;
+
+        for (int j = 0; j < n; j++) {
+            result[j] = product / nums[j];
+        }
+
+        return result;
     }
 }
+
 
 // 3. Suffix & Prefix Product
 class Solution {
@@ -61,3 +64,38 @@ class Solution {
         return ans;
     }
 }
+
+/**
+ * 240810
+ *
+ * # Understand
+ * 1. Does only one answer exist?
+ *
+ *
+ *
+ * # Match
+ * 1. Brute Force (TLE)
+ *      Tc: O(N^2)
+ *      SC: O(1)
+ * 2. Two Pts
+ *      TC: O(N)
+ *      SC: O(1)
+ *
+ * # Plan
+ * # Impliment
+ * # Review
+ * # Evaluate
+ *
+ * # Notes
+ * [Brute Force]
+ * 1. Initialize product to 1 and continuously multiply
+ * 2. How to exclude itself? -> if statement (i != j)
+ * 3. Fist for loop -> iterate array 'result'; Second for loop -> iterate elements in array 'nums'
+ *
+ * [Division] unsolved issue
+ *
+ * [Prefix & Suffix]
+ * 0. Initialize the result array as an array filled with 0s.
+ * 1. The prefix product array stores the product of all elements before each position.
+ * 2. The suffix product array stores the product of all elements after each position.
+ */
