56a7bf9cc963ffaf4069ff1a5159bd626b1e0153
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-08-10 15:44:43 +0800

    Today's Progress

--- Java/3. Hash Table/0169. Majority Element.java
+++ Java/3. Hash Table/0169. Majority Element.java
@@ -16,3 +16,34 @@ class Solution {
         return 0;
     }
 }
+
+/**
+ * 2408
+ *
+ * # Understand
+ * 1. Are there any requirement on time complexity?
+ * 2. If the element appears exactly n / 2 times, does it count?
+ *
+ *
+ *
+ * # Match
+ * 1. Brute Force
+ *      Tc:
+ *      SC:
+ * 2. HashMap
+ *      TC: O(N)
+ *      SC: O(N)
+ *
+ * # Plan
+ *  1. Using a for loop to iterate through the elements in the array nums and add (nums[i], freq) as a key-value pair in the HashMap.
+ *  2. Using an if statement to ensure that the key with the highest frequency occurs more than n / 2 times.
+ *
+ * # Implement
+ * # Review
+ * # Evaluate
+ *
+ * # Notes
+ * 1. How to initialize a new hashmap
+ * 2. map.getOrDefault -> can be used to solve frequency problem
+ *
+ */
