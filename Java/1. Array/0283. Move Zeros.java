8eab2fa34af1ec2bcbb9f7bd71b4c65cc068c12f
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-08-11 17:17:42 +0800

    Today's Progress

---
+++ Java/1. Array/0283. Move Zeros.java
@@ -0,0 +1,55 @@
+// class Solution {
+//     public void moveZeroes(int[] nums) {
+//         int j = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             if (nums[i] != 0) {
+//             int temp = nums[j];
+//             nums[j] = nums[i];
+//             nums[i] = temp;
+//             j++;
+//             }
+//         }
+//     }
+// }
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        for (int i = nums.length - 1; i >= index; i--) {
+            nums[i] = 0;
+        }
+    }
+}
+
+
+
+/**
+ * 240811
+ *
+ * # Understand
+ * 1. Is the input array guaranteed to contain at least one zero?
+ * 2. What should happen if all elements in the array are zeros?
+ *
+ *
+ * # Match
+ * 1. Brute Force
+ *      Tc: N/A
+ *      SC: N/A
+ * 2. Temp Pointer Method
+ *      TC:
+ *      SC: O(1)
+ *
+ * # Plan
+ * # Impliment
+ * # Review
+ * # Evaluate
+ *
+ * # Notes
+ * See notebook
+ */
