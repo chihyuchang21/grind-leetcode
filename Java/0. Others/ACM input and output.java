8b4fa2f4793f07264dabe5a88ae1d74c115b0b6c
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-09-05 11:47:35 -0500

    Today's Progress

---
+++ Java/0. Others/ACM input and output.java
@@ -0,0 +1,35 @@
+/*
+Input Format:
+
+        There are  lines of input, and each line contains a single integer.
+
+        [Sample Input]
+        42
+        100
+        125
+
+        [Sample Output]
+        42
+        100
+        125
+
+ */
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // Set up a new scanner
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("c: " + c);
+    }
+}
+
+
