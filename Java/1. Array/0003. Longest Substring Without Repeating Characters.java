98332e279c026e08afcba6ef3404944c40a6c988
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-09-25 14:05:57 -0500

    Today's Progress (BS + Sliding Window)

--- Java/1. Array/0003. Longest Substring Without Repeating Characters.java
+++ Java/1. Array/0003. Longest Substring Without Repeating Characters.java
@@ -1,20 +1,79 @@
+// // Brute Force: HashSet + Without Sliding Window
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         int maxLength = 0;
+
+//         for (int i = 0; i < n; i++) {
+//             HashSet<Character> set = new HashSet<>();
+
+//             for (int j = i; j < n; j++) { // Should start from "i"!
+//                 if (set.contains(s.charAt(j))) {
+//                     break;
+//                 }
+//                 // If char is not in the set, add it to the set!
+//                 set.add(s.charAt(j));
+
+//                 // Update maxLength
+//                 // j - i + 1: The length of the current valid substring
+//                 maxLength = Math.max(maxLength, j - i + 1);
+//             }
+//         }
+//         return maxLength;
+
+//     }
+// }
+
+
+// O(N) Solution: HashSet + Sliding Window
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        List<Character> subString = new ArrayList<>();
-        int largestLength = 0;
+        // 使用 HashSet 來記錄當前窗口內的字符
+        HashSet<Character> charSet = new HashSet<>();
+        int maxLength = 0; // 記錄最大子字串長度
+        int left = 0; // 左指針，初始為 0
 
+        // 右指針遍歷整個字串
         for (int right = 0; right < s.length(); right++) {
-            char currentChar = s.charAt(right);
+            char c = s.charAt(right);
 
-            if (subString.contains(currentChar)) {
-                int index = subString.indexOf(currentChar);
-                subString = subString.subList(index + 1, subString.size());
+            // 如果當前字符已經存在於窗口內，則移動左指針，直到移除重複的字符
+            while (charSet.contains(c)) {
+                charSet.remove(s.charAt(left)); // 從 Set 中移除左指針指向的字符
+                left++; // 移動左指針
             }
 
-            subString.add(currentChar);
-            largestLength = Math.max(largestLength, subString.size());
+            // 將當前字符加入到 Set 中
+            charSet.add(c);
+
+            // 更新最大子字串的長度
+            maxLength = Math.max(maxLength, right - left + 1);
         }
 
-        return largestLength;
+        return maxLength; // 返回最長的子字串長度
     }
 }
+
+
+
+// Discard
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         List<Character> subString = new ArrayList<>();
+//         int largestLength = 0;
+
+//         for (int right = 0; right < s.length(); right++) {
+//             char currentChar = s.charAt(right);
+
+//             if (subString.contains(currentChar)) {
+//                 int index = subString.indexOf(currentChar);
+//                 subString = subString.subList(index + 1, subString.size());
+//             }
+
+//             subString.add(currentChar);
+//             largestLength = Math.max(largestLength, subString.size());
+//         }
+
+//         return largestLength;
+//     }
+// }
--- Java/17. Binary Search/0875. Koko Eating Bananas.java
+++ Java/17. Binary Search/0875. Koko Eating Bananas.java
@@ -23,3 +23,31 @@ class Solution {
         return right;
     }
 }
+
+// 240925
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right = 1;
+
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+
+            if (hourSpent <= h) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return right;
+    }
+}
