4f0c93a539fcfc636bdd9b21828cfcd3623eeb86
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-09-29 12:40:50 -0500

    Sliding window is killing me

--- Java/1. Array/0424. Longest Repeating Character Replacement.java
+++ Java/1. Array/0424. Longest Repeating Character Replacement.java
@@ -18,3 +18,30 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        // 記錄每個字母出現的次數
+        int[] count = new int[26];
+        int left = 0, maxCount = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            // 將當前字母加入統計
+            count[s.charAt(right) - 'A']++;
+            // 更新當前窗口內出現最多的字母次數
+            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
+
+            // 如果窗口內需要替換的字母數量大於 k，縮小窗口
+            if (right - left + 1 - maxCount > k) {
+                // 縮小左邊界，減少對應字母的計數
+                count[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            // 更新最大子字串長度
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
