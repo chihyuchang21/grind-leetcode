4949af7b18562aac43934ea366dbdb303604a42f
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-09-30 15:04:58 -0500

    Dunno why I spent so much time and accomplished so little (sigh)

--- Java/2. LinkedList/0203. Remove Linked List Elements.java
+++ Java/2. LinkedList/0203. Remove Linked List Elements.java
@@ -73,3 +73,7 @@ class Solution {
         return dummy.next;
     }
 }
+
+// 在操作鏈表時，當使用 虛擬頭節點（dummy head node） 後，
+// 常常會在最後返回 dummy.next。這是因為虛擬頭節點本身並不屬於真正的鏈表數據，只是一個輔助節點。
+// 因此，我們需要返回真正的鏈表頭節點，而這個頭節點是由 dummy.next 指向的。
--- Java/2. LinkedList/0206. Reverse Linked List.java
+++ Java/2. LinkedList/0206. Reverse Linked List.java
@@ -21,7 +21,7 @@ class Solution {
         ListNode prev = null;
         ListNode cur = head;
         ListNode temp = null;
-        while (cur != null){
+        while (cur != null) {
             temp = cur.next; //保存下一個節點
             cur.next = prev;
             prev = cur;
@@ -35,7 +35,7 @@ class Solution {
         ListNode prev = null;
         ListNode cur = head;
         ListNode temp = null;
-        while (cur!= null) {
+        while (cur != null) {
             temp = cur.next; // 先儲存 cur 要往前的位置
             cur.next = prev; // 改變指向
             prev = cur; // prev 指針往前
@@ -58,6 +58,26 @@ class Solution {
                 prev = cur;
                 cur = temp;
             }
+            return prev;
+        }
+    }
+
+    // 4th 240930
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;  // 用來保存當前節點的前一個節點，最開始為 null，表示反轉後的最後一個節點
+            ListNode cur = head;   // 用來遍歷鏈表的當前節點，從 head 開始
+            ListNode temp = null;  // 用來臨時保存當前節點的下一個節點，以免反轉過程中丟失鏈表
+
+            // 遍歷整個鏈表，直到 cur 變為 null（即鏈表末尾）
+            while (cur != null) {
+                temp = cur.next;   // 保存當前節點的下一個節點
+                cur.next = prev;   // 反轉鏈接：將當前節點的 next 指向它的前一個節點
+                prev = cur;        // 移動 prev 到當前節點，準備下一次反轉
+                cur = temp;        // 移動 cur 到下一個節點（即剛才存的 temp）
+            }
+
+            // 當所有節點反轉後，prev 會指向反轉後的頭節點，因此返回 prev
             return prev;
         }
     }
