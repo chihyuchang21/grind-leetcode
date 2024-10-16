6bcf08cb9db0bb7e0f77997509833cf105d3ceb0
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-10-16 14:00:08 -0500

    Today's Progress (By Order - LL)

--- Java/2. LinkedList/0138. Copy List with Random Pointer.java
+++ Java/2. LinkedList/0138. Copy List with Random Pointer.java
@@ -67,3 +67,59 @@ class Solution {
         return oldToCopy.get(head);
     }
 }
+
+// 241016
+// Definition for a Node.
+// class Node {
+//     int val;
+//     Node next;
+//     Node random;
+
+//     public Node(int val) {
+//         this.val = val;
+//         this.next = null;
+//         this.random = null;
+//     }
+// }
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        // Step 1: 複製每個節點並將複製的節點插入到原節點後面
+        Node cur = head;
+        while (cur != null) {
+            Node newNode = new Node(cur.val);
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = newNode.next;
+        }
+
+        // Step 2: 複製 random 指針
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) {
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;  // 跳過新節點
+        }
+
+        // Step 3: 拆分鏈表
+        cur = head;
+        Node copyHead = head.next;
+        Node copyCur = copyHead;
+
+        while (cur != null) {
+            cur.next = cur.next.next;  // 恢復原鏈表的 next 指針
+            if (copyCur.next != null) {
+                copyCur.next = copyCur.next.next;  // 連接新鏈表的 next 指針
+            }
+            cur = cur.next;
+            copyCur = copyCur.next;
+        }
+
+        return copyHead;
+    }
+}
