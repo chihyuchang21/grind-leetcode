67e766180a58351e3b5c13d1de99fadea29d66c1
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-10-22 12:22:12 -0500

    Today's Progress

--- Java/3. Hash Table/0242. Valid Anagram.java
+++ Java/3. Hash Table/0242. Valid Anagram.java
@@ -22,3 +22,5 @@ class Solution {
         return true;
     }
 }
+
+// 241022
--- Java/3. Hash Table/0383. Ransom Note.java
+++ Java/3. Hash Table/0383. Ransom Note.java
@@ -1,3 +1,24 @@
-class Solution {
-    public
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        // 建立一個 HashMap 來記錄雜誌中每個字母的次數
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        // 計算雜誌中每個字母的出現次數
+        for (char c : magazine.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        // 檢查勒索信中的每個字母是否存在於雜誌中，且次數足夠
+        for (char c : ransomNote.toCharArray()) {
+            if (!charCount.containsKey(c) || charCount.get(c) == 0) {
+                // 如果字母不存在或次數不足，返回 false
+                return false;
+            }
+            // 使用一次該字母後，減少字母的次數
+            charCount.put(c, charCount.get(c) - 1);
+        }
+
+        // 所有字母都符合條件，返回 true
+        return true;
+    }
 }
