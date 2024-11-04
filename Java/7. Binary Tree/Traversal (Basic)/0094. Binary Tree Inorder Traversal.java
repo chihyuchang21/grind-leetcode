af96d747ce5653328d9321cdf2cc0fb760a88e0f
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-11-04 15:12:42 -0600

    Today's Progress (Binary Tree is the most important data structure!)

--- Java/7. Binary Tree/Traversal (Basic)/0094. Binary Tree Inorder Traversal.java
+++ Java/7. Binary Tree/Traversal (Basic)/0094. Binary Tree Inorder Traversal.java
@@ -14,6 +14,7 @@
  * }
  */
 
+// recursive
 class Solution {
     public List<Integer> result = new ArrayList<>();
 
@@ -30,5 +31,28 @@ class Solution {
         traversal(root.left);
         result.add(root.val);
         traversal(root.right);
+    }
+}
+
+// iteration
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()){
+            if (cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }else{
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return result;
     }
 }
--- Java/7. Binary Tree/Traversal (Basic)/0145. Binary Tree Postorder Traversal.java
+++ Java/7. Binary Tree/Traversal (Basic)/0145. Binary Tree Postorder Traversal.java
@@ -14,6 +14,7 @@
  * }
  */
 
+// recursive
 class Solution {
     public List<Integer> result = new ArrayList<>();
 
@@ -32,3 +33,26 @@ class Solution {
         result.add(root.val);
     }
 }
+
+// iteration
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.left != null){
+                stack.push(node.left);
+            }
+            if (node.right != null){
+                stack.push(node.right);
+            }
+        }
+        Collections.reverse(result);
+        return result;
+    }
