2291f618045a39ec546a492646cfd92db7eea1b6
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-11-06 14:32:29 -0600

    Today's Progress (Binary Tree is the most important data structure!)

--- Java/7. Binary Tree/0110. Balanced Binary Tree.java
+++ Java/7. Binary Tree/0110. Balanced Binary Tree.java
@@ -15,29 +15,18 @@
  */
 
 class Solution {
-    /**
-     * recursion
-     */
     public boolean isBalanced(TreeNode root) {
-        return getHeight(root) != -1;
+        if (root == null) return true;
+        if (Height(root) == -1) return false;
+        return true;
     }
 
-    private int getHeight(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        int leftHeight = getHeight(root.left);
-        if (leftHeight == -1) {
-            return -1;
-        }
-        int rightHeight = getHeight(root.right);
-        if (rightHeight == -1) {
-            return -1;
-        }
-        // 左右子樹高度差 > 1，return -1 表示已經不是 balanced tree
-        if (Math.abs(leftHeight - rightHeight) > 1) {
-            return -1;
-        }
+    public int Height(TreeNode root) {
+        if (root == null) return 0;
+        int leftHeight = Height(root.left);
+        int rightHeight = Height(root.right);
+        if (leftHeight == -1 || rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
         return Math.max(leftHeight, rightHeight) + 1;
     }
 }
--- Java/7. Binary Tree/0226. Invert Binary Tree.java
+++ Java/7. Binary Tree/0226. Invert Binary Tree.java
@@ -30,7 +30,28 @@ class Solution {
 
         return root;
     }
+}
 
-    // To-do: BFS
+// BFS iteration
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
+
+        return root;
+    }
 }
 
