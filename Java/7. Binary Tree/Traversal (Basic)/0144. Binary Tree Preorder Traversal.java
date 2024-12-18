8d1fc405a63f89aa9c2b0af3884c1002a1042269
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-12-18 11:04:13 -0600

    Today's Progress (binary tree)

--- Java/7. Binary Tree/Traversal (Basic)/0144. Binary Tree Preorder Traversal.java
+++ Java/7. Binary Tree/Traversal (Basic)/0144. Binary Tree Preorder Traversal.java
@@ -14,24 +14,24 @@
  * }
  */
 
-//class Solution {
-//    private List<Integer> result = new ArrayList<>();
-//
-//    public List<Integer> preorderTraversal(TreeNode root) {
-//        traversal(root);
-//        return result;
-//    }
-//
-//    private void traversal(TreeNode root) {
-//        if (root == null) {
-//            return;
-//        }
-//
-//        result.add(root.val);
-//        traversal(root.left);
-//        traversal(root.right);
-//    }
-//}
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        traversal(root);
+        return result;
+    }
+
+    private void traversal(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        result.add(root.val);
+        traversal(root.left);
+        traversal(root.right);
+    }
+}
 
 // Recursive
 class Solution {
