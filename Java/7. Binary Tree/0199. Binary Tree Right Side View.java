1ee12d2be8d88f34ef126c40ed151469e25a254e
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-12-19 20:56:49 -0600

    Today's Progress (still binary tree)

--- Java/7. Binary Tree/0199. Binary Tree Right Side View.java
+++ Java/7. Binary Tree/0199. Binary Tree Right Side View.java
@@ -54,6 +54,62 @@
  * }
  */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> result = new ArrayList<>();
+//         rightView(root, 0, result);
+//         return result;
+//     }
+
+//     private void rightView(TreeNode node, int depth, List<Integer> result) {
+//         // 基本條件：當節點為 null 時結束
+//         if (node == null) return;
+
+//         // 若當前深度與 result 大小相等，表示是第一次到達這個深度，加入該層的最右側節點
+//         if (depth == result.size()) {
+//             result.add(node.val);
+//         }
+
+//         // 優先遍歷右子樹，然後遍歷左子樹
+//         rightView(node.right, depth + 1, result);
+//         rightView(node.left, depth + 1, result);
+//     }
+// }
+
+
+// Iteration
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -76,3 +132,4 @@ class Solution {
         return result;
     }
 }
+
