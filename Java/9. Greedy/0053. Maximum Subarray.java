41ad3ce0ddf09a1b9cc480404c0926b428c4dfa9
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-12-31 21:17:18 -0600

    Today's Progress

--- Java/9. Greedy/0053. Maximum Subarray.java
+++ Java/9. Greedy/0053. Maximum Subarray.java
@@ -1,7 +1,10 @@
 // Kadane's Algorithm
 class Solution {
     public int maxSubArray(int[] nums) {
-        if (nums.length == 1) return nums[0];
+
+        if (nums.length == 1) {
+            return nums[0]
+        };
 
         int sum = 0;
         int max = Integer.MIN_VALUE;
