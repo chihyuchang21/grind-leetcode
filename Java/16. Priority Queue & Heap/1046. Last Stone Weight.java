a72da4f457c88732cf6d4a2098f72338ee985402
Chih-Yu Chang <chihyuchang21@gmail.com>
2024-12-31 22:43:21 -0600

    Today's Progress (2025!!!)

--- Java/16. Priority Queue & Heap/1046. Last Stone Weight.java
+++ Java/16. Priority Queue & Heap/1046. Last Stone Weight.java
@@ -1,5 +1,5 @@
 class Solution {
-
+// 用 MaxHeap 的技巧 -> 若不用 Comparator，用負數
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue();
         for (int stone : stones) maxHeap.add(-stone);
@@ -8,7 +8,41 @@ class Solution {
             int stone2 = maxHeap.remove();
             if (stone1 != stone2) maxHeap.add(stone1 - stone2);
         }
-        return maxHeap.size() != 0 ? (-maxHeap.remove()) : 0;
+
+        if (maxHeap.isEmpty()) {
+            return 0;
+        } else {
+            return maxHeap.remove();
+        }
+}
+
+// 用 Comparator 實現
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        // 使用 PriorityQueue 實現最大堆
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+
+        // 將所有石頭加入堆
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+
+        // 模擬石頭碰撞的過程
+        while (maxHeap.size() > 1) {
+            int stone1 = maxHeap.remove(); // 取出最大石頭
+            int stone2 = maxHeap.remove(); // 取出第二大石頭
+
+            if (stone1 != stone2) {
+                maxHeap.add(stone1 - stone2); // 差值放回堆中
+            }
+        }
+
+        // 返回結果
+        if (maxHeap.isEmpty()) {
+            return 0; // 堆空，返回 0
+        } else {
+            return maxHeap.remove(); // 堆中剩下最後一塊石頭
+        }
     }
 }
 
