7d4101d0a1751f3eea5edba45c4c05f92a55836b
Chih-Yu Chang <chihyuchang21@gmail.com>
2025-02-25 18:16:09 -0600

    Today's Progress

--- Java/1. Array/0003. Longest Substring Without Repeating Characters.java
+++ Java/1. Array/0003. Longest Substring Without Repeating Characters.java
@@ -1,57 +1,26 @@
-// // Brute Force: HashSet + Without Sliding Window
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         int n = s.length();
-//         int maxLength = 0;
-
-//         for (int i = 0; i < n; i++) {
-//             HashSet<Character> set = new HashSet<>();
-
-//             for (int j = i; j < n; j++) { // Should start from "i"!
-//                 if (set.contains(s.charAt(j))) {
-//                     break;
-//                 }
-//                 // If char is not in the set, add it to the set!
-//                 set.add(s.charAt(j));
-
-//                 // Update maxLength
-//                 // j - i + 1: The length of the current valid substring
-//                 maxLength = Math.max(maxLength, j - i + 1);
-//             }
-//         }
-//         return maxLength;
-
-//     }
-// }
-
-
-// O(N) Solution: HashSet + Sliding Window
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // 使用 HashSet 來記錄當前窗口內的字符
-        HashSet<Character> charSet = new HashSet<>();
-        int maxLength = 0; // 記錄最大子字串長度
-        int left = 0; // 左指針，初始為 0
+        HashSet<Character> set = new HashSet<>();
+        int maxLength = 0;
+        int left = 0;
 
-        // 右指針遍歷整個字串
+        if (s.length() == 0) {
+            return 0;
+        }
+
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
 
-            // 如果當前字符已經存在於窗口內，則移動左指針，直到移除重複的字符
-            while (charSet.contains(c)) {
-                left++; // 移動左指針
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
             }
 
-            // 將當前字符加入到 Set 中
-            charSet.add(c);
+            set.add(c);
 
-            // 更新最大子字串的長度
             maxLength = Math.max(maxLength, right - left + 1);
         }
 
-        return maxLength; // 返回最長的子字串長度
+        return maxLength;
     }
 }
-
-
-
