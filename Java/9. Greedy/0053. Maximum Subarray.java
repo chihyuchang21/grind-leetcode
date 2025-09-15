ad4a825465b74dc3be1ef9efd0984f503fcafeb8
Chih-Yu Chang <chihyuchang21@gmail.com>
2025-09-15 13:48:08 -0500

    Today's Progress

--- Java/9. Greedy/0053. Maximum Subarray.java
+++ Java/9. Greedy/0053. Maximum Subarray.java
@@ -23,17 +23,36 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-        dp[0] = nums[0];
+        dp[0] = nums[0]; // dp array 的意義是: 至此位(含)的 max subarray sum
         int result = dp[0];
 
         for (int i = 1; i < n; i++) {
-            dp[i] = Math.max(nums[i], dp[i-1] + nums[i]);
+            if (dp[i-1] > 0) { // 前面有幫助(>0)，則接上
+                dp[i] = dp[i-1] + nums[i];
+            } else { // 前一段拖累(<0)，重開
+                dp[i] = nums[i];
+            }
             result = Math.max(result, dp[i]);
         }
-
         return result;
     }
 }
 
 // TO-DO: Kadane’s Algorithm
-// https://chatgpt.com/share/68c489b7-61b8-800b-8947-9c995eb049a9
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int cur = nums[0];      // 相當於 dp[i]
+        int result = nums[0];   // 記錄歷史最大值
+
+        for (int i = 1; i < nums.length; i++) {
+            if (cur > 0) {
+                cur = cur + nums[i];  // 接上
+            } else {
+                cur = nums[i];        // 重開
+            }
+            result = Math.max(result, cur);
+        }
+
+        return result;
+    }
+}
